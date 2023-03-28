b8e980c941ec8a068d0302627bea3fd6136c1691
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-03-28 15:24:26 +0530

    implemented meta tags

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,23 @@ export default function Header() {
     return(
         <>
         <Head>
-        <title>Electrawise</title>
-        <meta name="description" content="Electrawise" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* Primary Meta Tags */}
+        <title>Electrawise | Solar Panels in Kerala | Save 80% on bills</title>
+        <meta name="title" content="Electrawise | Solar Panels in Kerala | Save 80% on bills" />
+        <meta name="description" content="Say goodbye to high energy bills with solar panels. Electrawise can help you save up to 80% on your energy bill!" />
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://solarpanels.electrawise.in/" />
+        <meta property="og:title" content="Electrawise | Solar Panels in Kerala | Save 80% on bills" />
+        <meta property="og:description" content="Say goodbye to high energy bills with solar panels. Electrawise can help you save up to 80% on your energy bill!" />
+        <meta property="og:image" content="/meta-img.png" />
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://solarpanels.electrawise.in/" />
+        <meta property="twitter:title" content="Electrawise | Solar Panels in Kerala | Save 80% on bills" />
+        <meta property="twitter:description" content="Say goodbye to high energy bills with solar panels. Electrawise can help you save up to 80% on your energy bill!" />
+        <meta property="twitter:image" content="/meta-img.png" />
         </Head>
         </>
     )
