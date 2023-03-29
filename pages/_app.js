e4afbaec921ff0c9eae925299d114cea164a1a93
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-03-29 11:29:23 +0530

    gtag implementation

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,23 @@ import '../styles/globals.css'
 export default function App({ Component, pageProps }) {
   return(
     <>
+    <Script
+      strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-C6LZG46DLG`}
+      />
+
+  <Script id="google-analytics-script" strategy="afterInteractive">
+    {`
+
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-C6LZG46DLG' , {
+        page_path: window.location.pathname,
+      });
+
+      `}
+
+  </Script>
       <NavBar />
       <Component {...pageProps} />
       <Footer />
