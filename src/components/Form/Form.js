4e699898bfb02e1f85629ea605d6f778ee061c09
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-03-29 11:11:00 +0530

    testing

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -64,7 +64,7 @@ export default function Form() {
         setFullName('');
         setPhoneNumber('');
         setLocation('');
-         router.push("/thank-you");
+         router.reload("/");
 
     }
     // Function to validate email 
