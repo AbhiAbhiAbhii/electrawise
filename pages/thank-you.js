f595eae1d4ea41c00342276233bc0640ec1fde21
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-03-29 13:25:29 +0530

    thankyou tag added

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -1,16 +1,29 @@
 /* eslint-disable @next/next/no-img-element */
+import Script from "next/script"
 export default function Thanks() {
     return(
-      <section className="thankYou">
-        <div className="thankYouContent">
+
+      <><Script
+        strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-C6LZG46DLG`} /><Script id="google-analytics-script" strategy="afterInteractive">
+          {`
+
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('event', 'conversion', {'send_to': 'AW-11117880810/k1qHCNjA7JQYEOrLtbUp'});
+
+      `}
+
+        </Script><section className="thankYou">
+          <div className="thankYouContent">
             <div className="thanksContainer">
-                <img style={{height:'100%', width:'100%', objectFit:'contain'}} src="/thanks.svg" alt=""/>
+              <img style={{ height: '100%', width: '100%', objectFit: 'contain' }} src="/thanks.svg" alt="" />
             </div>
             <div className="thanksTextContainer">
-                <div className="thanksText inter-b"><p color="#000">Thank you for submitting</p></div>
-                <div className="thanksSubText inter-r"><p style={{color:'grey'}}>Our customer executive will get in touch with you within 48hrs</p></div>
+              <div className="thanksText inter-b"><p color="#000">Thank you for submitting</p></div>
+              <div className="thanksSubText inter-r"><p style={{ color: 'grey' }}>Our customer executive will get in touch with you within 48hrs</p></div>
             </div>
-        </div>
-      </section>
+          </div>
+        </section></>
     )
 }
